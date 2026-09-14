bad2303f2f49c90c8817ceb6085edae9cf8d86be
6ixtyight <6ixtyight@gmail.com>
2026-09-14 15:48:38 +0100

    added new lines

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log(
   `Oh ye ALIEN,Welcome to the MULTIVERSE!`
 );
 
+const bill = prompt("Enter your bill amount: ");
+
+const tipPercentage = prompt("Enter the tip percentage (e.g., 15 for 15%): ");
